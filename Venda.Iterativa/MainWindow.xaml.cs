2bea1abf864182945b18acd866dd9a6f70c1349c
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2023-03-02 22:20:09 -0300

    Implementação MVVM

--- Venda.Iterativa/MainWindow.xaml.cs
+++ Venda.Iterativa/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Venda.Iterativa.ViewModel;
 
 namespace Venda.Iterativa
 {
@@ -23,6 +24,7 @@ namespace Venda.Iterativa
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowViewModel();
         }
 
         private void btnOlaMundo_Click(object sender, RoutedEventArgs e)
---
+++ Venda.Iterativa/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Venda.Iterativa.Classes;
+
+namespace Venda.Iterativa.ViewModel
+{
+    internal class MainWindowViewModel : AbstractViewModel
+    {
+        public MainWindowViewModel() : base("UMFG | Home") { }
+    }
+}
